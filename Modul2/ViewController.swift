c6dfc2504739f8cedbb75a7b27603cc6098c61f4
Юрий <145429474+yuriIosDev@users.noreply.github.com>
2024-01-09 12:58:03 +0300

    Initial Commit

---
+++ Modul2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modul2
+//
+//  Created by Юрий Кузнецов on 09.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
